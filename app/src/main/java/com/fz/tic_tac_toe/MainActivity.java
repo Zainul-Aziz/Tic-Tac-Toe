d707c38e813c74146d8f0e028dae2e42d1d39e97
Zainul-Aziz <64695371+Zainul-Aziz@users.noreply.github.com>
2020-06-02 21:58:35 +0530

    Update MainActivity.java

--- app/src/main/java/com/fz/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/fz/tic_tac_toe/MainActivity.java
@@ -5,9 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT =2000;
+    private long backPress;
+    private Toast backToast;
+    @Override
+    public void onBackPressed() {
+        if (backPress + 1 > System.currentTimeMillis())
+        {
+            backToast.cancel();
+            super.onBackPressed();
+            backPress = System.currentTimeMillis();
+            return;
+        }
+        else
+        {
+            backToast = Toast.makeText(getBaseContext(),"Please Wait!",Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+        backPress = System.currentTimeMillis();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
